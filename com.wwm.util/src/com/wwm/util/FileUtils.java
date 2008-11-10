28489496a645ade08d91c4fe284d51f2eafd8c78
nupstone <nupstone@d93b9ff3-e951-0410-ba53-e55492e652b6>
2008-11-10 21:32:02 +0000

    Add support for writing and reading large objects to a GZipped file

--- com.wwm.util/src/com/wwm/util/FileUtils.java
+++ com.wwm.util/src/com/wwm/util/FileUtils.java
@@ -11,12 +11,112 @@
 package com.wwm.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class FileUtils {
 
 	static private Logger log;
 	
+	/**
+	 * WARNING: Currently throws Error (should be IOException
+	 * @param fileName
+	 * @return
+	 * @throws java.lang.Error
+	 */
+	static public Object readObjectFromGZip(String fileName) {
+		// FIXME: Do finally { blah.close().. etc }
+		File file = new File(fileName);
+		
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("File missing: " + file);
+			throw new Error(e);
+		}
+		GZIPInputStream gzis;
+		try {
+			gzis = new GZIPInputStream(fis);
+		} catch (IOException e) {
+			System.out.println("Error reading from " + file);
+			System.out.println("Error opening GZIP input stream: " + e);
+			throw new Error(e);
+		}
+		ObjectInputStream ois;
+		try {
+			ois = new ObjectInputStream(gzis);
+		} catch (IOException e) {
+			System.out.println("Error reading from " + file);
+			System.out.println("Error opening ObjectInputStream: " + e);
+			throw new Error(e);
+		}
+		
+		Object obj;
+		try {
+			obj = ois.readObject();
+		} catch (IOException e) {
+			System.out.println("Error reading from " + file + ": " + e);
+			throw new Error(e);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Internal Error reading from " + file + ": " + e);
+			throw new Error(e);
+		} catch (ClassCastException e) {
+			System.out.println("File Format Error reading from " + file + ": " + e);
+			throw new Error(e);
+		}
+		return obj;
+	}
+
+	static public void writeObjectToGZip(String outFileName, Object obj) throws IOException {
+		// FIXME: Do finally { blah.close().. etc }
+		File outfile = new File(outFileName);
+		outfile.delete();
+		File parent = new File(outfile.getParent());
+		parent.mkdirs();
+	
+		// Write file out
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(outfile);
+		} catch (FileNotFoundException e) {
+			System.out.println("Error opening " + outfile + " for output: " + e.getMessage());
+			throw e;
+		}
+				
+		GZIPOutputStream gzos = null;
+		try {
+			gzos = new GZIPOutputStream(fos);
+		} catch (IOException e) {
+			System.out.println("Error creating GZIPOutputStream: " + e.getMessage());
+			throw e;
+		}
+		
+		ObjectOutputStream oos = null;
+		try {
+			oos = new ObjectOutputStream(gzos);
+		} catch (IOException e) {
+			System.out.println("Error creating ObjectOutputStream: " + e.getMessage());
+			throw e;
+		}
+		
+		try {
+			oos.writeObject(obj);
+			oos.flush();
+			oos.close();
+		} catch (IOException e) {
+			System.out.println("Error while writing: " + e.getMessage());
+			throw e;
+		}
+	}
+
 	public static void setLogger(Logger log) {
 		FileUtils.log = log;
 	}
