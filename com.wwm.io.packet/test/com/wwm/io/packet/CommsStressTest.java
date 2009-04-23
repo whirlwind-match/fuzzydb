36fc70ae40f14da5a4ce16ea199b1de8d5ce4d30
nupstone <nupstone@d93b9ff3-e951-0410-ba53-e55492e652b6>
2009-04-23 16:13:14 +0000

    Add a test (and it's failing at that!)

---
+++ com.wwm.io.packet/test/com/wwm/io/packet/CommsStressTest.java
@@ -0,0 +1,122 @@
+package com.wwm.io.packet;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.util.Collection;
+import java.util.Iterator;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.wwm.io.packet.exceptions.NotListeningException;
+import com.wwm.io.packet.impl.DummyCli;
+import com.wwm.io.packet.layer1.Server;
+import com.wwm.io.packet.layer1.ServerImpl;
+import com.wwm.io.packet.layer2.SourcedMessage;
+import com.wwm.io.packet.messages.Message;
+
+public class CommsStressTest {
+	protected static final String defaultAddress = "127.0.0.1";
+	protected static final int serverPort = 5002;
+	
+	ClassLoaderInterface cli = new DummyCli();
+	
+	@SuppressWarnings("serial")
+	private static class TestMessage extends Message {
+
+		int value;
+		byte[] bytes = new byte[10];
+		
+		public TestMessage(int value) {
+			super(0,0);
+			this.value = value;
+		}
+
+	}
+	
+	private void doConnectLoops(Server server, int loops) throws IOException, NotListeningException
+	{
+		for (int loop = 0; loop < loops; loop++) {
+			// Make client
+			InetSocketAddress isa = new InetSocketAddress(defaultAddress, serverPort);	// connect to main adaptor
+			CommsStack client = new TCPStack(SocketChannel.open(isa), cli);
+			
+			TestMessage tm = new TestMessage(42);
+			client.getMessageInterface().send(tm);
+			
+			Collection<SourcedMessage> messages = server.waitForMessage(1000);
+			
+			Iterator<SourcedMessage> i = messages.iterator();
+			TestMessage rx = (TestMessage)i.next().getMessage();
+			
+			Assert.assertEquals(tm.value, rx.value);
+			
+			client.getMessageInterface().close();
+		}		
+		
+	}
+
+	private void doMessageLoops(Server server, int loops) throws IOException, NotListeningException
+	{
+		// Make client
+		InetSocketAddress isa = new InetSocketAddress(defaultAddress, serverPort);	// connect to main adaptor
+		CommsStack client = new TCPStack(SocketChannel.open(isa), cli);
+
+		for (int loop = 0; loop < loops; loop++) {
+			
+			TestMessage tm = new TestMessage(42);
+			client.getMessageInterface().send(tm);
+			
+			Collection<SourcedMessage> messages = server.waitForMessage(1000);
+			
+			Iterator<SourcedMessage> i = messages.iterator();
+			TestMessage rx = (TestMessage)i.next().getMessage();
+			
+			Assert.assertEquals(tm.value, rx.value);
+			
+		}		
+		client.getMessageInterface().close();
+		
+	}
+
+	@Test 
+	public void testManyMessages() throws IOException, NotListeningException {
+		final int loops = 10000;
+		
+		// Make server
+		ServerImpl server = new ServerImpl(cli);
+		server.listen(new InetSocketAddress(serverPort));	// listen on main adaptor
+		
+		doMessageLoops(server, loops);
+		
+		long start = System.currentTimeMillis();
+		doMessageLoops(server, loops);
+		long duration = System.currentTimeMillis() - start;
+		System.out.println(loops + " messages took " + duration + "ms. Time per message: " + (float)duration/loops + "ms");
+		server.close();
+	}
+	
+	@Test 
+	public void testManyConnects() throws IOException, NotListeningException {
+		// This test will fail with a high number of loops due to the sockets hanging around.
+		// By default Win XP seems to only allow about 5000 sockets, and they hang around for 240 secs in the TIMED_WAIT state.
+		// See http://support.microsoft.com/kb/314053/ for info on how to change TcpTimedWaitDelay and possibly TcpNumConnections. 
+		
+		final int loops = 500;
+		
+		// Make server
+		ServerImpl server = new ServerImpl(cli);
+		server.listen(new InetSocketAddress(serverPort));	// listen on main adaptor
+		
+		doConnectLoops(server, loops);
+		
+		long start = System.currentTimeMillis();
+		doConnectLoops(server, loops);
+		long duration = System.currentTimeMillis() - start;
+		System.out.println(loops + " connect loops took " + duration + "ms");
+		server.close();
+	}
+}
