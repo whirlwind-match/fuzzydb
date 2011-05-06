179cc32d3ab3531bee8a0b76e277351974de9480
Neale Upstone <neale.upstone@opencredo.com>
2011-05-06 22:45:58 +0100

    Revamp of TxLogIterator - still not happy, but happier

--- com.wwm.db.server/src/main/java/com/wwm/db/internal/server/txlog/TxLogIterator.java
+++ com.wwm.db.server/src/main/java/com/wwm/db/internal/server/txlog/TxLogIterator.java
@@ -11,6 +11,7 @@
 package com.wwm.db.internal.server.txlog;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -20,19 +21,32 @@ import java.util.Iterator;
  */
 public class TxLogIterator implements Iterable<File> {
 
-	private File txDir;
-	private long startVersion;
-	private boolean allowNonExact;
+	private final File txDir;
+	private final long startVersion;
+	private final boolean allowNonExact;
 
+	
 	private class IteratorImpl implements Iterator<File> {
-		ArrayList<File> candidates;
+		private final ArrayList<File> ascendingCandidates = new ArrayList<File>();
 
 		/** Version at which the tx log is starting playback */
-		private long highestDbVer = -1;
+		private long highestVersionApplied = -1;
 
 		public IteratorImpl() {
-			candidates = new ArrayList<File>();
-			Collections.addAll(candidates, txDir.listFiles());
+			FileFilter fileFilter = new FileFilter() {
+				
+				public boolean accept(File file) {
+					if (file.isDirectory()){
+						return false;
+					}
+					
+					String filename = file.getName();
+					int index = filename.indexOf('_');
+					return (index >= 2 && filename.startsWith("t"));
+				}
+			};
+			Collections.addAll(ascendingCandidates, txDir.listFiles(fileFilter));
+			Collections.sort(ascendingCandidates, TransactionFileNameComparator.getInstance());
 		}
 
 		
@@ -40,13 +54,13 @@ public class TxLogIterator implements Iterable<File> {
 		 * Note: May say has next when it's not exactly sure.  next() may return null in those circumstances
 		 */
 		public boolean hasNext() {
-			return candidates.size() > 0;
+			return ascendingCandidates.size() > 0;
 		}
 
 		public File next() {
-			if (candidates.size() == 0) return null;
+			if (ascendingCandidates.size() == 0) return null;
 			
-			File file = removeBestCandidate(candidates);
+			File file = removeBestCandidate(ascendingCandidates);
 
 			return file;
 		}
@@ -60,40 +74,45 @@ public class TxLogIterator implements Iterable<File> {
 		 * On txLog playback, we want to apply transaction logs that include the transactions from the
 		 * version of the currently loaded repository, forwards.
 		 * 
-		 * Algo: 
+		 * OLD! Algo: 
 		 *  - keep track of highest version that is lower than current db version (as that might be the one)
 		 *  - if version == dbversion, return it, it's exact match (what we normally expect)
 		 *  - if version < dbversion but not greater than highestDbVer, 
 		 *  		then remove it (as all it's transactions are in repos or have been applied)
 		 *  - if version < dbversion and > highestDbVer, update highestDbVer, and remember what file it is
 		 *  After have been through all, return the best found
+		 *  
+		 *  NEW: If we find startVersion, then remove it and return it
 		 */
 		private File removeBestCandidate(ArrayList<File> candidates) {
-			File best = null;
-			Iterator<File> i = candidates.iterator();
-			while (i.hasNext()) {
-				File file = i.next();
-				long version = getDbVerFromFilename(file);
-				if (version == -1) {
-					continue; // skip dirs and files we don't care about
+			
+			for (Iterator<File> iterator = candidates.iterator(); iterator.hasNext();) {
+				File file = iterator.next();
+				
+				long version = TransactionFileNameComparator.getDbVerFromFilename(file);
+				if (version == -1 || version < highestVersionApplied) {
+					iterator.remove();
+					continue; // skip if uninteresting or not high enough 
 				}
 				
+				// IF we have a match for our start point, then apply this transaction log
+				// and record 
 				if (version == startVersion) {
-					i.remove();
-					highestDbVer = version;
+					iterator.remove();
+					highestVersionApplied = version;
 					return file;
-				} else if (version < startVersion) {
-					// Mod by neale: original didn't return anything if there wasn't one matching the repos.  This way we do find a version
-					// but then rely on playback to skip transactions until at the correct point
-					if (version > highestDbVer && allowNonExact){
-						highestDbVer = version;
-						best = file;
-					} 
-				} else {
-					i.remove(); // remove anything not high enough
 				}
+				
+				// If we've applied something, then next one higher is our next
+				if (highestVersionApplied >= 0 && version > highestVersionApplied) {
+					iterator.remove();
+					highestVersionApplied = version;
+					return file;
+				}
+				iterator.remove();
 			}
-			return best;
+			// If all transaction logs are old, then we can just start
+			return null;
 		}
 	}
 
@@ -113,27 +132,6 @@ public class TxLogIterator implements Iterable<File> {
 
 	public Iterator<File> iterator() {
 		return new IteratorImpl();
-	}
-
-	/**
-	 * Extract first digits from between initial 't' and '_'. e.g. 123 from filename "t123_dontcare".
-	 * @return long 
-	 */
-	static private long getDbVerFromFilename(File file) {
-		if ( file.isDirectory() ) {
-			return -1;
-		}
-		String filename = file.getName();
-		int index = filename.indexOf('_');
-		if (index < 2 || !filename.startsWith("t")) {
-			return -1; // some random file we don't want
-		}
-
-		try {
-			return Long.parseLong(filename.substring(1, index));
-		} catch (NumberFormatException e) { 
-			return -1; // do nothing: we return -1 below.
-		}
 	}
 	
 }
