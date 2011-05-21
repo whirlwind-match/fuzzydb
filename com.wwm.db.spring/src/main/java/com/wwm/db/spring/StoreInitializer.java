530db5ed7639544e1f3fc369401feeecc4af64c3
Neale Upstone <neale.upstone@opencredo.com>
2011-05-21 19:59:53 +0100

    Add StoreInitializer InitializingBean to do the equiv of jdbc:initialize
    Simply a case of <bean class="com.wwm.db.spring.StoreInitializer"
    p:resource="classpath:propertyMatchingConfig.xml" />
    
    The store is autowired (for now)

--- com.wwm.db.spring/src/main/java/com/wwm/db/spring/StoreInitializer.java
+++ com.wwm.db.spring/src/main/java/com/wwm/db/spring/StoreInitializer.java
@@ -1,5 +1,6 @@
 package com.wwm.db.spring;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.DefaultResourceLoader;
@@ -8,10 +9,13 @@ import org.springframework.core.io.Resource;
 import com.wwm.attrs.WWConfigHelper;
 import com.wwm.context.JVMAppListener;
 import com.wwm.db.Store;
-import com.wwm.indexer.IndexerFactory;
+import com.wwm.db.core.LogFactory;
 
 public class StoreInitializer implements InitializingBean {
 
+	
+	static private final Logger log = LogFactory.getLogger(StoreInitializer.class);
+	
 	@Autowired
 	private Store store;
 	
@@ -24,10 +28,12 @@ public class StoreInitializer implements InitializingBean {
 		// FIXME: Remove everything to do with the next 3 lines!
         JVMAppListener.getInstance().setSingleSession();
         JVMAppListener.getInstance().preRequest();
-        IndexerFactory.setCurrentStoreUrl("wwmdb:/" + store.getStoreName()); // TODO: Should be able to ask store for it's URL... ?
+//        IndexerFactory.setCurrentStoreUrl("wwmdb:/" + store.getStoreName()); // TODO: Should be able to ask store for it's URL... ?
         
 		Resource resource = new DefaultResourceLoader().getResource(resourcePath);
 		WWConfigHelper.updateScorerConfig(store, resource.getInputStream());
+		
+		log.info("Loaded scorer config: {}", resourcePath);
 	}
 
 	public void setResource(String resource) {
