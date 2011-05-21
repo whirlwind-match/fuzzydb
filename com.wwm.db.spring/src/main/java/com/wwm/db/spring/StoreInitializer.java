ca103a7c634d96b44b8d6a1365460d31aea6f4cf
Neale Upstone <neale@nealeupstone.com>
2011-05-21 19:37:37 +0100

    WIP: Build breaking commit to com.wwm.db.spring - so can work on it at home

---
+++ com.wwm.db.spring/src/main/java/com/wwm/db/spring/StoreInitializer.java
@@ -0,0 +1,36 @@
+package com.wwm.db.spring;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+
+import com.wwm.attrs.WWConfigHelper;
+import com.wwm.context.JVMAppListener;
+import com.wwm.db.Store;
+import com.wwm.indexer.IndexerFactory;
+
+public class StoreInitializer implements InitializingBean {
+
+	@Autowired
+	private Store store;
+	
+//	@Autowired
+//	@Qualifier("resource")
+	private String /* TODO: Resource */ resourcePath;
+	
+	
+	public void afterPropertiesSet() throws Exception {
+		// FIXME: Remove everything to do with the next 3 lines!
+        JVMAppListener.getInstance().setSingleSession();
+        JVMAppListener.getInstance().preRequest();
+        IndexerFactory.setCurrentStoreUrl("wwmdb:/" + store.getStoreName()); // TODO: Should be able to ask store for it's URL... ?
+        
+		Resource resource = new DefaultResourceLoader().getResource(resourcePath);
+		WWConfigHelper.updateScorerConfig(store, resource.getInputStream());
+	}
+
+	public void setResource(String resource) {
+		this.resourcePath = resource;
+	}
+}
---
+++ com.wwm.db.spring/src/test/java/com/wwm/db/spring/ExampleSpringFuzzyTest.java
@@ -0,0 +1,86 @@
+package com.wwm.db.spring;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wwm.db.DataOperations;
+import com.wwm.db.GenericRef;
+import com.wwm.db.annotations.Key;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:/annotation-driven-aspectj-tx-context.xml"})
+public class ExampleSpringFuzzyTest {
+	
+	static public class IndexedMap implements Serializable {  // FIXME : Make implement IWhirlwind blah
+		private static final long serialVersionUID = 1L;
+
+		private final @Key(unique=true) String key;
+		
+		private final HashMap<String, Object> stuff = new HashMap<String, Object>();
+		
+		public IndexedMap(String key) {
+			this.key = key;
+		}
+		
+		public String getKey() {
+			return key;
+		}
+		
+		public void put(String key, Object value) {
+			stuff.put(key, value);
+		}
+		
+		public Object get(String key) {
+			return stuff.get(key);
+		}
+	}
+	
+	
+	@Autowired
+	private DataOperations dataOperations;
+	
+	@Test 
+	public void createObjectSucceedInAtTransactionalViaInjectedDataOps(){
+
+		// Create an item
+		IndexedMap originalItem = new IndexedMap("Hello");
+		originalItem.put("height", Integer.valueOf(181));
+		GenericRef<IndexedMap> ref = insertSomething(originalItem);
+
+		// Retrieve by ref
+		IndexedMap item = retrieveByRef(ref);
+		assertThat(item.getKey(), equalTo("Hello"));
+		assertThat(item.get("height"),equalTo((Object)Integer.valueOf(181)));
+		
+		// Do an index lookup
+		IndexedMap item2 = retrieveByKey("Hello");
+		assertThat(item2.getKey(), equalTo("Hello"));
+		assertThat(item2.get("height"),equalTo((Object)Integer.valueOf(181)));
+		
+	}
+
+	@Transactional 
+	private GenericRef<IndexedMap> insertSomething(IndexedMap item) {
+		return dataOperations.createGeneric(item);
+	}
+	
+	@Transactional(readOnly=true)
+	private <T> T retrieveByRef(GenericRef<T> ref) {
+		return dataOperations.retrieve(ref);
+	}
+
+	@Transactional(readOnly=true)
+	private IndexedMap retrieveByKey(String key) {
+		return dataOperations.retrieve(IndexedMap.class, "key", key);
+	}
+}
