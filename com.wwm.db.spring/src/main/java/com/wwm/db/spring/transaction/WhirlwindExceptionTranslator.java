e7b92ec2320b81089b58c469bb5f343bbeae6a95
Neale Upstone <neale@nealeupstone.com>
2011-06-16 16:17:18 +0100

    Add handling of UnknownObjectException to exception translator

--- com.wwm.db.spring/src/main/java/com/wwm/db/spring/transaction/WhirlwindExceptionTranslator.java
+++ com.wwm.db.spring/src/main/java/com/wwm/db/spring/transaction/WhirlwindExceptionTranslator.java
@@ -1,14 +1,17 @@
 package com.wwm.db.spring.transaction;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.support.PersistenceExceptionTranslator;
-
-import com.wwm.db.core.exceptions.ArchException;
+import com.wwm.db.exceptions.UnknownObjectException;
 
 public class WhirlwindExceptionTranslator implements
 		PersistenceExceptionTranslator {
 
 	public DataAccessException translateExceptionIfPossible(RuntimeException ex) {
+		if (ex instanceof UnknownObjectException) {
+			return new EmptyResultDataAccessException(1);
+		}
 //		if (ex instanceof ArchException) {
 
 		return null;
