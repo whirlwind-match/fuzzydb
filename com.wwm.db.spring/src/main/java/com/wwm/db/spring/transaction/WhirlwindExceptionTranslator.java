8c1c0891c3be845e1fd3d8699e111a03a08b341e
Neale Upstone <neale@nealeupstone.com>
2011-06-17 18:16:26 +0100

    Add cause to translated exceptions [Spring]

--- com.wwm.db.spring/src/main/java/com/wwm/db/spring/transaction/WhirlwindExceptionTranslator.java
+++ com.wwm.db.spring/src/main/java/com/wwm/db/spring/transaction/WhirlwindExceptionTranslator.java
@@ -3,17 +3,33 @@ package com.wwm.db.spring.transaction;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.support.PersistenceExceptionTranslator;
+
+import com.wwm.db.core.exceptions.ArchException;
 import com.wwm.db.exceptions.UnknownObjectException;
 
 public class WhirlwindExceptionTranslator implements
 		PersistenceExceptionTranslator {
 
 	public DataAccessException translateExceptionIfPossible(RuntimeException ex) {
+		DataAccessException e = doTranslate(ex);
+		if (e == null) {
+			return null;
+		}
+		
+		e.initCause(ex);
+		return e;
+		
+	}
+	
+	protected DataAccessException doTranslate(RuntimeException ex) {
+		if (!(ex instanceof ArchException)) {
+			return null;
+		}
+
 		if (ex instanceof UnknownObjectException) {
 			return new EmptyResultDataAccessException(1);
 		}
-//		if (ex instanceof ArchException) {
 
-		return null;
+		return null; // can't translate
 	}
 }
