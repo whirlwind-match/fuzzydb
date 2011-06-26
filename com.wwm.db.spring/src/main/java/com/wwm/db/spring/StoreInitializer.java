001ff6905247385b6b86240cc3ca47f4930bca2e
Neale Upstone <neale.upstone@opencredo.com>
2011-06-26 19:19:00 +0100

    Update StoreInitializer to use PathMatchingResourcePatternResolver for wildcards
    Can now use <fuzzy:initialize match-styles="classpath*:*MatchingConfig.xml" />
    to initialise using all found configs.

--- com.wwm.db.spring/src/main/java/com/wwm/db/spring/StoreInitializer.java
+++ com.wwm.db.spring/src/main/java/com/wwm/db/spring/StoreInitializer.java
@@ -3,8 +3,8 @@ package com.wwm.db.spring;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import com.wwm.attrs.WWConfigHelper;
 import com.wwm.context.JVMAppListener;
@@ -29,10 +29,13 @@ public class StoreInitializer implements InitializingBean {
         JVMAppListener.getInstance().preRequest();
 //        IndexerFactory.setCurrentStoreUrl("wwmdb:/" + store.getStoreName()); // TODO: Should be able to ask store for it's URL... ?
         
-		Resource resource = new DefaultResourceLoader().getResource(resourcePath);
-		WWConfigHelper.updateScorerConfig(store, resource.getInputStream());
-		
-		log.info("Loaded scorer config: {}", resourcePath);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources(resourcePath);
+
+        for (Resource resource : resources) {
+        	WWConfigHelper.updateScorerConfig(store, resource.getInputStream());
+        	log.info("Loaded match style from: {}", resource.getURL());
+		}
 	}
 
 	@Autowired
