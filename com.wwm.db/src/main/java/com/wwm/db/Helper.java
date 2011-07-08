97f96ab760ccfa73f622253ecbd685b03a261d95
Neale Upstone <neale.upstone@opencredo.com>
2011-07-08 17:45:40 +0100

    Move to Java 1.6 and fix single clash that resulted

--- com.wwm.db/src/main/java/com/wwm/db/Helper.java
+++ com.wwm.db/src/main/java/com/wwm/db/Helper.java
@@ -21,7 +21,7 @@ public interface Helper {
 	 * @return a Ref for the Object
 	 * @throws UnknownObjectException
 	 */
-	<E> Ref getRef(E obj) throws UnknownObjectException;
+	//<E> Ref getRef(E obj) throws UnknownObjectException;
 	
 	/**Gets the version number of the specified object.
 	 * This returns the version of the presented object, not the version of the newest variant in the database.
