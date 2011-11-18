ff862a56082a1c723407d30374542246edb96f25
Neale Upstone <neale.upstone@opencredo.com>
2011-11-18 18:40:46 +0000

    Check SubjectMatchQuery params for null

--- com.wwm.db.spring/src/main/java/com/wwm/db/spring/repository/SubjectMatchQuery.java
+++ com.wwm.db.spring/src/main/java/com/wwm/db/spring/repository/SubjectMatchQuery.java
@@ -1,5 +1,7 @@
 package com.wwm.db.spring.repository;
 
+import org.springframework.util.Assert;
+
 /**
  * A SubjectMatchQuery looks for the best matches for a provided subject, according to the
  * requested match style.
@@ -14,6 +16,9 @@ public class SubjectMatchQuery<QT> implements AttributeMatchQuery<QT> {
 
 	public SubjectMatchQuery(QT attributes,
 			String matchStyle, int maxResults) {
+		Assert.notNull(attributes, "attributes must not be null");
+		Assert.notNull(matchStyle, "matchStyle must not be null");
+		
 		this.attributes = attributes;
 		this.matchStyle = matchStyle;
 		this.maxResults = maxResults;
