54ac1d6fef5430e53f3d307cc5cb66860a2a10c8
Neale Upstone <neale@nealeupstone.com>
2012-08-28 17:40:32 +0100

    Github #86: Add files I missed 2 commits ago

---
+++ server/src/test/java/org/fuzzydb/server/EmbeddedClientFactoryTest.java
@@ -0,0 +1,44 @@
+package org.fuzzydb.server;
+
+import static org.junit.Assert.assertNotNull;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.fuzzydb.client.Client;
+import org.fuzzydb.client.Store;
+import org.fuzzydb.client.WWMDBProtocolHander;
+import org.fuzzydb.server.EmbeddedClientFactory;
+import org.junit.Test;
+
+
+public class EmbeddedClientFactoryTest {
+
+	
+	@Test
+	public void createStoreAgainstEmbeddedDatabaseShouldReturnNonNullStore() throws IOException {
+		
+		Client client = EmbeddedClientFactory.getInstance().createClient();
+		
+		Store store = client.createStore("store@" + System.currentTimeMillis());
+
+		assertNotNull(store);
+		
+		client.deleteStore(store.getStoreName());
+		EmbeddedClientFactory.getInstance().shutdownDatabase(); // TODO: do this via appropriate lifecycle hook for the application
+	}
+	
+	@Test 
+	public void openUrlForEmbeddedStore() throws MalformedURLException {
+		final String storeName = "store@" + System.currentTimeMillis();
+		final String url = "wwmdb:/" + storeName;
+		
+		Store store = EmbeddedClientFactory.getInstance().openStore(WWMDBProtocolHander.getAsURL(url));
+		
+		assertNotNull(store);
+		
+		Client client = EmbeddedClientFactory.getInstance().createClient();
+		client.deleteStore(storeName);
+		EmbeddedClientFactory.getInstance().shutdownDatabase(); // TODO: do this via appropriate lifecycle hook for the application
+		
+	}
+}
