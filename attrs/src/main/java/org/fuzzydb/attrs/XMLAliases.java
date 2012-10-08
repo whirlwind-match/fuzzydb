9499aa59bec8cf312b8222468dd05b8daff14918
Neale Upstone <neale@nealeupstone.com>
2012-10-08 18:03:43 +0100

    Update XMLAliases to provide default impl class for IDimensions

--- attrs/src/main/java/org/fuzzydb/attrs/XMLAliases.java
+++ attrs/src/main/java/org/fuzzydb/attrs/XMLAliases.java
@@ -4,25 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.fuzzydb.attrs.bool.BooleanScorer;
 import org.fuzzydb.attrs.bool.BooleanPriority;
+import org.fuzzydb.attrs.bool.BooleanScorer;
 import org.fuzzydb.attrs.dimensions.DimensionPriority;
+import org.fuzzydb.attrs.dimensions.Dimensions;
 import org.fuzzydb.attrs.enums.EnumDefinition;
 import org.fuzzydb.attrs.enums.EnumExclusiveScorerExclusive;
 import org.fuzzydb.attrs.enums.EnumExclusiveScorerPreference;
-import org.fuzzydb.attrs.enums.SingleEnumPriority;
 import org.fuzzydb.attrs.enums.EnumSingleValueScorer;
 import org.fuzzydb.attrs.enums.MultiEnumScorer;
 import org.fuzzydb.attrs.enums.OptionsScorer;
 import org.fuzzydb.attrs.enums.SeatsScorer;
+import org.fuzzydb.attrs.enums.SingleEnumPriority;
 import org.fuzzydb.attrs.internal.ScoreConfiguration;
 import org.fuzzydb.attrs.location.LocationAndRangeScorer;
 import org.fuzzydb.attrs.location.PathDeviationScorer;
 import org.fuzzydb.attrs.location.VectorDistanceScorer;
-import org.fuzzydb.attrs.simple.FloatRangePreferenceScorer;
 import org.fuzzydb.attrs.simple.FloatPriority;
+import org.fuzzydb.attrs.simple.FloatRangePreferenceScorer;
 import org.fuzzydb.attrs.simple.SimilarFloatValueScorer;
 import org.fuzzydb.attrs.simple.WeightedSumScorer;
+import org.fuzzydb.dto.dimensions.IDimensions;
 import org.fuzzydb.util.AsymptoticScoreMapper;
 import org.fuzzydb.util.LinearScoreMapper;
 
@@ -30,9 +32,9 @@ import com.thoughtworks.xstream.XStream;
 
 public class XMLAliases {
 
-	
+
 	static private Map<String, Class<?>> scorerAliases = new HashMap<String,Class<?>>();
-	
+
 	static {
 		initScorerAliases();
 	}
@@ -43,10 +45,10 @@ public class XMLAliases {
         scorerAliases.put("EnumMatchScorer", EnumExclusiveScorerExclusive.class);
         scorerAliases.put("EnumScoresMapScorer", EnumExclusiveScorerPreference.class);
         scorerAliases.put("EnumSingleValueScorer", EnumSingleValueScorer.class);
-        
+
         scorerAliases.put("MultiEnumScorer", MultiEnumScorer.class);
         scorerAliases.put("OptionsScorer", OptionsScorer.class);
-        
+
         scorerAliases.put("SimilarFloatValueScorer", SimilarFloatValueScorer.class);
         scorerAliases.put("FloatRangePreferenceScorer", FloatRangePreferenceScorer.class);
         scorerAliases.put("WeightedSumScorer", WeightedSumScorer.class);
@@ -61,7 +63,7 @@ public class XMLAliases {
         scorerAliases.put("LinearScoreMapper", LinearScoreMapper.class);
         scorerAliases.put("AsymptoticScoreMapper", AsymptoticScoreMapper.class);
 	}
-	
+
 	public static Map<String, Class<?>> getScorerAliases() {
 		return scorerAliases;
 	}
@@ -97,16 +99,18 @@ public class XMLAliases {
 	    xStream.useAttributeFor(ManualIndexStrategy.class, "name");
 	    // ensure contained elements are added to splitConfigurations
 	    xStream.addImplicitCollection(ManualIndexStrategy.class, "priorities");
-	
+
 	    // Split config stuff
 	    xStream.alias("Priority", AttributePriority.class);
 	    xStream.alias("BooleanPriority", BooleanPriority.class);
-	    xStream.alias("DimensionPriority", DimensionPriority.class);
 	    xStream.alias("SingleEnumPriority", SingleEnumPriority.class);
 	    // xStream.alias("MultiEnumPriority", MultiEnumPriority.class);
 	    xStream.alias("FloatPriority", FloatPriority.class);
 	    // xStream.alias("RangePriority", RangePriority.class);
+
+	    xStream.alias("DimensionPriority", DimensionPriority.class);
+	    xStream.addDefaultImplementation(Dimensions.class, IDimensions.class);
 	}
-	
-	
+
+
 }
