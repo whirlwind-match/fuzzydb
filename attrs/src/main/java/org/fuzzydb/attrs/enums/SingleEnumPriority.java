a297426beab9ef227fd2a68b4c009a9e4fce2e32
Neale Upstone <neale@nealeupstone.com>
2012-10-16 08:36:51 +0100

    Clean up

--- attrs/src/main/java/org/fuzzydb/attrs/enums/SingleEnumPriority.java
+++ attrs/src/main/java/org/fuzzydb/attrs/enums/SingleEnumPriority.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2008 Whirlwind Match Limited. All rights reserved.
  *
  * This is open source software; you can use, redistribute and/or modify
- * it under the terms of the Open Software Licence v 3.0 as published by the 
+ * it under the terms of the Open Software Licence v 3.0 as published by the
  * Open Source Initiative.
  *
  * You should have received a copy of the Open Software Licence along with this
@@ -36,19 +36,4 @@ public class SingleEnumPriority extends AttributePriority {
 	public float getPriority() {
 		return priority;
 	}
-	
-	// Migrate from old version that used to contain an EnumDefinition
-	// This can be removed when all is migrated.
-	// FIXME: Remove this when migrated
-//	private void readObject( ObjectInputStream in ) throws IOException, ClassNotFoundException {
-//		GetField fields = in.readFields();
-//		priority = fields.get("priority", 1f );
-//		
-//		try {
-//				EnumDefinition def = (EnumDefinition) fields.get("def", null);
-//			size = def.size();
-//		} catch (IllegalArgumentException e) {
-//			size = fields.get( "size", 32);
-//		}
-//	}
 }
